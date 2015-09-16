3f23a239084f7d691502e62582ba8081c943f2eb
Berto Yáñez <berto@ber.to>
2015-09-16 09:46:19 +0200

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        path: './dist',
+        filename: 'bundle.js',
+        publicPath: "/dist/"
+    },
+    resolve: {
+      modulesDirectories: ['node_modules', 'bower_components', 'modules']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+       ]
+    }
+};
